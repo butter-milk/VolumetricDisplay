1680117e2506315e64221af7ecb068f82d86d176
butter-milk <kevinvandeglind@gmail.com>
2023-12-29 13:05:13 +0100

    added rotation parameter

--- VolumetricDisplay.py
+++ VolumetricDisplay.py
@@ -3,21 +3,28 @@ import numpy as np
 class VolumetricDisplay:
     width,height,center = 0,0,0
     rps: int #rotations per second
+    rotation: int #axis around which to rotate
     display = None
     
-    # in general, humans see between 30 to 60 fps, so we just are going to assume a nice 30. Might add a feature to edit this though
     # we assume that we rotate around the z axis, might make this adjustable as well, but for now we choose to work with it
-    def __init__(self,width,height,center = 0, rps = 10) -> None:
-        self.width = width
-        self.height = height
+    def __init__(self,width,height,center = 0, rps = 10, rotation=2) -> None:
         self.rps = rps
         self.center = center 
-        self.display = np.zeros(shape=(height,width))
+        self.rotation = rotation if rotation in [0,1,2] else 2
+        if rotation==2:
+            self.width = width 
+            self.height = height
+        else: 
+            #if we do not rotate around the z-axis, it is reasonable to assume that the z- and x-axis (or y respectively) have the same dimension
+            #so now width will be the differing axis
+            self.width = height 
+            self.height = width
         pass
 
     def display_static(self, image):
         #image: 3d numpy array
         #returns a list of images through which the user can loop with even time intervals between them, deltaT = 1/rps
+        image = np.swapaxes(image,self.rotation,2)
         left = -self.center
         right = self.width - self.center
         image_list = [None for _ in range(self.rps)]
